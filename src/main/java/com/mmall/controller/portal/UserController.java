c41085d9d8786d4822c8a7a8b9bc6029fec91100
lx520 <740707954@qq,com>
2017-11-14 23:52:19 +0800

    2017年11月14日 23:52:03

---
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -0,0 +1,23 @@
+package com.mmall.controller.portal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/user/")
+public class UserController {
+    /**
+     * 用户登录
+     * @param username
+     * @param password
+     * @return
+     */
+    @RequestMapping(value = "login.do", method = RequestMethod.POST)
+    @ResponseBody
+    public Object login(String username, String password) {
+        // controller -> service
+        return null;
+    }
+}
---
+++ src/main/java/com/mmall/service/impl/UserServiceImpl.java
@@ -0,0 +1,18 @@
+package com.mmall.service.impl;
+
+import com.mmall.common.ServerResponse;
+import com.mmall.dao.UserMapper;
+import com.mmall.pojo.User;
+import com.mmall.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UserServiceImpl implements IUserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public ServerResponse<User> login(String username, String password) {
+        return null;
+    }
+}
